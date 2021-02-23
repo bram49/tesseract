fe5d662979f817a75b1a194f797211b718780459
bram49 <schut49@gmail.com>
2021-02-23 17:56:11 +0000

    Added Plotly map

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import pandas as pd
 from dash.dependencies import Input, Output
-import folium
+import plotly.graph_objects as go
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 # Start the app
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+quakes = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/earthquakes-23k.csv')
+
+fig = go.Figure(go.Densitymapbox(lat=quakes.Latitude, lon=quakes.Longitude, z=quakes.Magnitude, radius=10))
+fig.update_layout(mapbox_style="stamen-terrain", mapbox_center_lon=180)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
+
 app.layout = html.Div(children=[
     html.H1('Tesseract'),
-    html.Iframe(id='map',
-                srcDoc = open('base_map.html', 'r').read(),
-                width= '100%',
-                style={'height':'80vh'}),
+    html.Div(className="worldmap", children=[
+        dcc.Graph(className="worldmap", figure=fig)
+    ]),
     dbc.FormGroup(
         [
             dbc.Label("Slider", html_for="slider"),
